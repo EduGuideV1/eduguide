674522cd893a60863a3d23c680544ae30aa62a81
hamza <hamza55bourbab@gmail.com>
2024-02-19 15:28:35 +0000

    add some css to the formule

--- resources/views/formule/stage_one.blade.php
+++ resources/views/formule/stage_one.blade.php
@@ -14,21 +14,23 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="font-sans antialiased w-full flex flex-col items-center">
-        <div class="w-full max-w-[1422px]">
-            @include('partials.nav')
-        </div>
-    <div class="container">
-        <form action="/stage-one" method="POST">
+    <div class="w-full max-w-[1422px]">
+        @include('partials.nav')
+    </div>
+    <div class="container mx-auto my-10 px-5 ">
+        <form action="/stage-one" method="POST" class="bg-white p-5 rounded shadow">
             @csrf
-            <label for="full_name">Full Name:</label>
-            <input type="text" id="full_name" name="full_name" required>
-        
-            <label for="phone_number">Phone Number:</label>
-            <input type="text" id="phone_number" name="phone_number" required>
-        
-            <button type="submit">Next</button>
+            <label for="full_name" class="block mb-2">Full Name:</label>
+            <input type="text" id="full_name" name="full_name" required class=" w-200 p-2 border border-gray-300 rounded">
+
+            <label for="phone_number" class="block mt-5 mb-2">Phone Number:</label>
+            <input type="text" id="phone_number" name="phone_number" required class=" w-200 p-2 border border-gray-300 rounded"><br>
+
+            <button type="submit" class="mt-5 bg-blue-500 text-white px-4 py-2 rounded">Next</button>
         </form>
     </div>
 </body>
+
 </html>
